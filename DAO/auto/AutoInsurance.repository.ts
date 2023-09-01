8bae521d74b397817a7ee00a2b00505a6069ced8
Carolinne Silva <carolinne.silva@compasso.com.br>
2023-09-01 11:55:04 -0300

    ref: change directory error catcher handling

--- DAO/auto/AutoInsurance.repository.ts
+++ DAO/auto/AutoInsurance.repository.ts
@@ -1,5 +1,4 @@
-import { AWSError } from "aws-sdk";
-import { AutoInsuranceModel } from "../../Models/Insurance.model";
+import { FormattedAutoInsuranceModel } from "../../Models/Insurance.model";
 import { MessageError } from "../../Utils/errors/errorMessages.enum";
 import { DynamoDocumentClient } from "../../Infra/dynamo/config";
 import { IAutoInsuranceRepository } from "../../Utils/interfaces/IAutoInsuranceRepository.interface";
@@ -7,63 +6,55 @@ import { IAutoInsuranceRepository } from "../../Utils/interfaces/IAutoInsuranceR
 const TableName = `${process.env.AUTO_INSURANCE_TABLE}`;
 
 export class AutoInsuranceRepository implements IAutoInsuranceRepository {
-  async create(data: AutoInsuranceModel): Promise<AutoInsuranceModel> {
-    try {
-      const params = {
-        TableName,
-        Item: {
-          id: data.id,
-        },
-      };
+  async create(
+    data: FormattedAutoInsuranceModel
+  ): Promise<FormattedAutoInsuranceModel> {
+    const params = {
+      TableName,
+      Item: {
+        id: data.id,
+        title: data.title,
+        use_terms: data.use_terms,
+        description: data.description,
+        value: data.value,
+        company: data.company,
+        createdAt: data.createdAt,
+        updatedAt: data.updatedAt,
+        product_id: data.product_id,
+      },
+    };
 
-      await DynamoDocumentClient.put(params).promise();
-      return data;
-    } catch (error) {
-      throw new Error(MessageError.CREATE_INSURANCE_ERROR);
-    }
+    await DynamoDocumentClient.put(params).promise();
+    return data;
   }
 
-  async findAll(): Promise<AutoInsuranceModel[]> {
-    try {
-      const params = {
-        TableName,
-      };
+  async findAll(): Promise<FormattedAutoInsuranceModel[]> {
+    const params = {
+      TableName,
+    };
 
-      const { Items: id } = await DynamoDocumentClient.scan(params).promise();
-      return id as AutoInsuranceModel[];
-    } catch (error) {
-      throw new Error(MessageError.FIND_INSURANCE_ERROR);
-    }
+    const { Items: id } = await DynamoDocumentClient.scan(params).promise();
+    return id as FormattedAutoInsuranceModel[];
   }
 
   async update(id: string): Promise<void> {
-    try {
-      const params = {
-        TableName,
-        Key: {
-          id: `${id}`,
-        },
-      };
+    const params = {
+      TableName,
+      Key: {
+        id: `${id}`,
+      },
+    };
 
-      await DynamoDocumentClient.update(params).promise();
-    } catch (error) {
-      throw new Error(MessageError.UPDATE_INSURANCE_ERROR);
-
-    }
+    await DynamoDocumentClient.update(params).promise();
   }
   async delete(id: string): Promise<void> {
-    try {
-      const params = {
-        TableName,
-        Key: {
-          id: `${id}`,
-        },
-      };
+    const params = {
+      TableName,
+      Key: {
+        id: `${id}`,
+      },
+    };
 
-      await DynamoDocumentClient.delete(params).promise();
-    } catch (error) {
-      throw new Error(MessageError.UPDATE_INSURANCE_ERROR);
-
-    }
+    await DynamoDocumentClient.delete(params).promise();
   }
 }
--- Utils/errors/errorMessages.enum.ts
+++ Utils/errors/errorMessages.enum.ts
@@ -3,5 +3,6 @@ export enum MessageError {
   DROP_TABLE_ERROR = "An error occurred while trying to delete the table",
   CREATE_INSURANCE_ERROR = "An error occurred while creating",
   FIND_INSURANCE_ERROR = "An error occurred while searching insurance",
-  UPDATE_INSURANCE_ERROR= "An error occurred while updating data"
+  UPDATE_INSURANCE_ERROR = "An error occurred while updating data",
+  DELETE_INSURANCE_ERROR = "An error ocurred while deleting insurance",
 }
